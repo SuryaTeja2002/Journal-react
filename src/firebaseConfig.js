c84a0e138c3a7a5a59c7198248b7d2ee1c134850
Surya Teja <92043974+SuryaTeja2002@users.noreply.github.com>
2024-10-19 14:24:56 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,19 +12,20 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 const database = getDatabase(app);
 
-// Define and export the signInWithGoogle function
 const signInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(auth, googleProvider);
+
     return result.user;
   } catch (error) {
     console.error("Error signing in with Google:", error);
+
+    alert("Sign-in failed. Please try again. Error: " + error.message);
   }
 };
 
